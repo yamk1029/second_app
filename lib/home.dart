9c59301720dffa43c2927685fa58c247cbda17cf
yamk1029 <yamk1029@gmail.com>
2025-04-09 22:33:31 +0545

    nothing, just http served

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
