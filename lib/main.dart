f7bba87fc95f82402aea824dedca98fd7c9e4172
yamk1029 <yamk1029@gmail.com>
2025-04-17 23:24:25 +0545

    todo frontend

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
       home: const Home(),
     );
   }
---
+++ lib/study.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Study extends StatefulWidget {
+  const Study({super.key});
+
+  @override
+  State<Study> createState() => _StudyState();
+}
+
+class _StudyState extends State<Study> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
